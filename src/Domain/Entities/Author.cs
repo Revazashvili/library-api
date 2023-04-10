cce75b3694974a710593d1a6b54f70973f06ab92
Revazashvili <levanrevazashvili@gmail.com>
2023-04-10 11:33:28 +0400

    add domain entities

---
+++ src/Domain/Entities/Author.cs
@@ -0,0 +1,3 @@
+﻿namespace Domain.Entities;
+
+public record Author(int Id, string FirstName, string LastName);
---
+++ src/Domain/Entities/Book.cs
@@ -0,0 +1,3 @@
+﻿namespace Domain.Entities;
+
+public record Book(int Id,string Title,string Description,Author Author);
