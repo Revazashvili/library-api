d5ab21599343618a029bf489a93537ab521a652f
Revazashvili <levanrevazashvili@gmail.com>
2023-04-10 21:18:13 +0400

    add sample feature

---
+++ src/Application/Common/Wrappers/ICommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.Common.Wrappers;
+
+public interface ICommand<out T> : IRequest<IResponse<T>> { }
+
+public interface ICommandHandler<in TRequest, TResponse> :
+    IRequestHandler<TRequest, IResponse<TResponse>> 
+    where TRequest : ICommand<TResponse> { }
---
+++ src/Application/Features/AddAuthor.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Models;
+using Application.Common.Wrappers;
+using Domain.Entities;
+
+namespace Application.Features;
+
+public static class AddAuthor
+{
+    public record Command(string FirstName, string LastName) : ICommand<Author>;
+    
+    public class Handler : ICommandHandler<Command,Author>
+    {
+        public Task<IResponse<Author>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
